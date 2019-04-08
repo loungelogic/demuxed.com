dda22dbda3255c42a10b09206792944ca20c70c9
meyginix <meyginix@gmail.com>
2019-04-08 16:45:08 +0300

    Got rid of twitter

--- src/components/HomePage/TweetsSection/TweetsSectionContainer.jsx
+++ src/components/HomePage/TweetsSection/TweetsSectionContainer.jsx
@@ -5,20 +5,21 @@ import TweetsSection from './TweetsSection';
 import { extractNodes } from '../../../helpers/data';
 
 const TweetsSectionContainer = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allTweet {
-          edges {
-            node {
-              tweetId: id_str
-            }
-          }
-        }
-      }
-    `}
-    render={data => <TweetsSection tweets={extractNodes(data.allTweet)} />}
-  />
+  // <StaticQuery
+  //   query={graphql`
+  //     query {
+  //       allTweet {
+  //         edges {
+  //           node {
+  //             tweetId: id_str
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `}
+  //   render={data => <TweetsSection tweets={extractNodes(data.allTweet)} />}
+  // />
+  <TweetsSection tweets={[]} />
 );
 
 export default TweetsSectionContainer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/common/Layout';
 import Hero from '../components/HomePage/Hero';
 import NewsSection from '../components/HomePage/NewsSection';
 import HomePageEventsSection from '../components/HomePage/EventsSection';
-import TweetsSection from '../components/HomePage/TweetsSection';
+// import TweetsSection from '../components/HomePage/TweetsSection';
 import FeaturedVideo from '../components/HomePage/FeaturedVideo';
 // import HomePageJobsSection from '../components/HomePage/JobsSection';
 import InvolvementSection from '../components/HomePage/InvolvementSection';
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <Hero />
     <NewsSection />
     <HomePageEventsSection />
-    <TweetsSection />
+    {/* <TweetsSection /> */}
     <FeaturedVideo />
     {/* <HomePageJobsSection /> */}
     <InvolvementSection />
